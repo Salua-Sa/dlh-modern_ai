9e2eed8775a25d7a4555a6989cb3fb2cb2efa0c3
Salua-Sa <slnulx5@gmail.com>
2026-07-10 13:20:01 +0200

    Add task 6. Plot Target Distribution

---
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This module visualizes churn class distribution in a DataFrame
+"""
+import matplotlib.pyplot as plt
+
+
+def plot_churn_distribution(df):
+    """
+    Plots the distribution of the Churn column.
+
+    Args:
+        df: The pandas DataFramecontaining a Churn column.
+
+    Returns:
+        None
+    """
+    plt.figure(figsize=(12, 8))
+
+    counts = df["Churn"].value_counts().reindex(["No", "Yes"])
+
+    plt.bar(
+        counts.index,
+        counts.values,
+        color=["skyblue", "salmon"]
+        )
+
+    plt.show()
+
+    return None
