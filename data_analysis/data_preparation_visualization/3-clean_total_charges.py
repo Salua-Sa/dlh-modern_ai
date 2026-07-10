c3840854a4b7dfe2ecc3590c3666a099f2d62695
Salua-Sa <slnulx5@gmail.com>
2026-07-10 10:17:35 +0200

    Add task 3. Dropping vs Replacing vs Imputation

---
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+This module handles missing values in a DataFrame.
+"""
+
+
+def clean_total_charges(df, method='drop'):
+    """
+    Handles missing values in the totalCharges column.
+
+    Args:
+        df: The pandas DataFrame to modify.
+        method: The strategy to use: drop, median, or impute.
+
+    Returns:
+        The modified DataFrame.
+    """
+
+    df = df.copy()
+    if method == 'drop':
+        df = df.dropna(subset=['TotalCharges'])
+    elif method == 'median':
+        df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
+    elif method == "impute":
+        df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges'] * df['tenure'])
+
+    return df
