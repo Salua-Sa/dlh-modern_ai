388aa1b437ac6ecfbf7476dda79d34564caffab4
Salua-Sa <slnulx5@gmail.com>
2026-07-10 10:20:12 +0200

    Fix pycodestyle 3. Dropping vs Replacing vs Imputation

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -20,8 +20,10 @@ def clean_total_charges(df, method='drop'):
     if method == 'drop':
         df = df.dropna(subset=['TotalCharges'])
     elif method == 'median':
-        df['TotalCharges'] = df['TotalCharges'].fillna(df['TotalCharges'].median())
+        df['TotalCharges'] = df['TotalCharges'].fillna(
+            df['TotalCharges'].median())
     elif method == "impute":
-        df['TotalCharges'] = df['TotalCharges'].fillna(df['MonthlyCharges'] * df['tenure'])
+        df['TotalCharges'] = df['TotalCharges'].fillna(
+            df['MonthlyCharges'] * df['tenure'])
 
     return df
