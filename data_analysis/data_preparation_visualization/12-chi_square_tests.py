003f440bad6b508afed597865305a69e8c3ae976
Salua-Sa <slnulx5@gmail.com>
2026-07-11 20:34:58 +0200

    Add task 12-chi_square_tests.py

---
+++ data_analysis/data_preparation_visualization/12-chi_square_tests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+This module performs chi-square tests for categorical features, using SciPy.
+"""
+import pandas as pd
+from scipy import stats
+
+
+def chi_square_tests(df):
+    """
+    Computes chi-square p-values
+for categorical columns against Churn
+
+    Args:
+        df: The pandas DataFrame
+    containing Churn and categorical columns.
+
+    Returns:
+        A dictionary mapping feature names to p-values.
+    """
+    dic_result = {}
+
+    for column in df.columns:
+        if df[column].dtype == "object" and column != "Churn":
+            table = pd.crosstab(df[column], df["Churn"])
+
+            test_result = stats.chi2_contingency(table)
+
+            chi2 = test_result[0]
+            p_value = test_result[1]
+            dof = test_result[2]
+            expected = test_result[3]
+
+            dic_result[column] = float(p_value)
+    return dic_result
