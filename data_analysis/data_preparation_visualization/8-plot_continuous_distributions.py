13d9abaa31792f921365d8c53b960e77b57c3582
Salua-Sa <slnulx5@gmail.com>
2026-07-11 15:45:38 +0200

    Fix task 8. Plot Continuous Distributions

--- data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
+++ data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
@@ -19,10 +19,13 @@ def plot_continuous_distributions(df, columns_to_plot=None):
         None
     """
     if columns_to_plot is None:
-        columns_to_plot = df.select_dtypes(include="number").columns
+        columns_to_plot = df.select_dtypes(include=np.number).columns
 
     n_cols = len(columns_to_plot)
     fig, axes = plt.subplots(n_cols, 2, figsize=(10, 3*n_cols))
+
+    if n_cols == 1:
+        axes = axes.reshape(1, -1)
 
     for i, column in enumerate(columns_to_plot):
         data = df[column].dropna()
@@ -34,12 +37,11 @@ def plot_continuous_distributions(df, columns_to_plot=None):
             edgecolor='black'
             )
         kde = stats.gaussian_kde(data)
-        x_values = np.linspace(data.min(), data.max(), 1000)
+        x_values = np.linspace(data.min(), data.max(), 100)
         axes[i][0].plot(
             x_values,
             kde(x_values),
-            color="red",
-            linestyle="--")
+            "r--")
         axes[i][0].set_title(f"{column} Histogram + KDE")
 
         if column == "TotalCharges":
