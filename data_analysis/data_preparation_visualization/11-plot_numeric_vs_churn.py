0442dbd70fa285095250c01d90c5bc167a3f1f4d
Salua-Sa <slnulx5@gmail.com>
2026-07-11 17:23:44 +0200

    Add task 11-plot_numeric_vs_churn.py

---
+++ data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+This module compares continuous numeric feature distributions by churn.
+"""
+from turtle import title
+
+import matplotlib.pyplot as plt
+
+
+def plot_numeric_vs_churn(df, col):
+    """
+        Plots a numeric column distribution
+        grouped by Churn.
+
+    Args:
+        df: The pandas DataFrame
+    containing a Churn column.
+        col: The numeric column
+    to compare by Churn.
+
+    Returns:
+        None
+    """
+    plt.figure(figsize=(12, 8))
+
+    churn_no = df[df["Churn"] == "No"][col]
+    churn_yes = df[df["Churn"] == "Yes"][col]
+
+    plt.hist(
+        [churn_no, churn_yes],
+        bins=30,
+        label=["No", "Yes"]
+        )
+
+    plt.title(f"{col} Distribution by Churn")
+    plt.xlabel(col)
+    plt.legend(title="Churn")
+
+    plt.show()
+
+    return None
