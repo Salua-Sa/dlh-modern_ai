9e1985685367a68c53f6acb0a2eeeb8c15b2d445
Salua-Sa <slnulx5@gmail.com>
2026-07-12 15:38:10 +0200

    Fix task  8-plot_continuous_distributions.py

--- data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
+++ data_analysis/data_preparation_visualization/8-plot_continuous_distributions.py
@@ -19,7 +19,7 @@ def plot_continuous_distributions(df, columns_to_plot=None):
         None
     """
     if columns_to_plot is None:
-        columns_to_plot = df.select_dtypes(include=np.number).columns.tolist()
+        columns_to_plot = df.select_dtypes(include=np.number).columns
 
     n_cols = len(columns_to_plot)
     fig, axes = plt.subplots(n_cols, 2, figsize=(10, 3*n_cols))
@@ -37,7 +37,7 @@ def plot_continuous_distributions(df, columns_to_plot=None):
             edgecolor='black'
             )
         kde = stats.gaussian_kde(data)
-        x_values = np.linspace(data.min(), data.max(), 100)
+        x_values = np.linspace(data.min(), data.max(), 200)
         axes[i, 0].plot(
             x_values,
             kde(x_values),
