b660e62e204e87710a5f8d79f0e4a1a03b549764
Salua-Sa <slnulx5@gmail.com>
2026-07-13 21:40:42 +0200

    Add task 0. Fetch HTML

---
+++ data_analysis/web_scraping/0-fetch_html.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""This module fetches a web page and returns its HTML as text.
+"""
+import requests
+
+
+def fetch_html(url, headers = None, timeout = 10):
+    """
+    Fetches a web page.
+
+    Args:
+        url: The page to retrieve.
+        headers: An optional dict of HTTP headers.
+        timeout: The number of seconds to wait before abording.
+
+    Return:
+        The HTML content.
+    """
+    response = requests.get(
+        url,
+        headers=headers,
+        timeout=timeout
+        )
+    response.raise_for_status()
+    return response.text
