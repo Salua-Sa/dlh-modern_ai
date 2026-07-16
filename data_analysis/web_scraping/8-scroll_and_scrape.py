3fd0a5938df835226cb235169db940732981e050
Salua-Sa <slnulx5@gmail.com>
2026-07-16 08:20:03 +0200

    Add task 8-scroll_and_scrape.py

---
+++ data_analysis/web_scraping/8-scroll_and_scrape.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""This module scrolls and extracts all products
+from a JS‐rendered infinite‐scroll page.
+"""
+import time
+from selenium import webdriver
+
+
+def scroll_and_scrape(url, scroll_pause=2.0):
+    """
+    Opens a detail page for one product.
+
+    Args:
+        url: The login page.
+        scroll_pause: waiting time.
+
+    Return:
+         A dictionary.
+    """
+
+    options = webdriver.ChromeOptions()
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    last_height = driver.execute_script(
+        "return document.body.scrollHeight")
+
+    while True:
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+
+        time.sleep(scroll_pause)
+
+        new_height = driver.execute_script(
+            "return document.body.scrollHeight")
+
+        if (new_height == last_height):
+            break
+
+        last_height = new_height
+
+    products = driver.find_elements(
+        "css selector",
+        "div.thumbnail"
+        )
+
+    unique_products_dicts = {}
+
+    for product in products:
+        title = product.find_element(
+            "css selector",
+            "a.title").get_attribute("title")
+        price = product.find_element(
+            "css selector",
+            "h4.price").text
+        description = product.find_element(
+            "css selector",
+            "p.description").text
+        rating = len(product.find_elements(
+            "css selector",
+            ".ratings .ws-icon.ws-icon-star"))
+        product_key = (title, price)
+        unique_products_dicts[product_key] = {
+            "title": title,
+            "price": price,
+            "description": description,
+            "rating": rating}
+
+    driver.quit()
+    return list(unique_products_dicts.values())
