b841397cd4d187d0f376543d2642475cb2c653c2
Salua-Sa <slnulx5@gmail.com>
2026-08-13 22:19:25 +0200

    Fix task 4-weight_initialization.py

--- deep_learning/enhancing_dl_models/4-weight_initialization.py
+++ deep_learning/enhancing_dl_models/4-weight_initialization.py
@@ -23,7 +23,7 @@ def build_model_initializer_by_activation(input_dim, hidden_units, activation):
     Returns:
         model: A Keras model with the described architecture.
     """
-    if activation in "sigmoid" or activation in "tanh":
+    if activation in ["sigmoid", "tanh"]:
         initializer = keras.initializers.GlorotUniform()
         acttivation_func = activation
     elif activation == "relu":
