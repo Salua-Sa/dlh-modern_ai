dabe4ef2360572b801d8d6d31ab70c9bbef00f2d
Salua-Sa <slnulx5@gmail.com>
2026-08-23 15:23:16 +0200

    Fix task 2-bottleneck_block.py

--- deep_learning/cv_architectures/2-bottleneck_block.py
+++ deep_learning/cv_architectures/2-bottleneck_block.py
@@ -52,6 +52,7 @@ def bottleneck_block(x, filters, stride=1, downsample=False, name=None):
         kernel_size=(3, 3),
         strides=stride,
         padding='same',
+        use_bias=False,
         name=f'{name}_conv2' if name else None)(x)
 
     x = keras.layers.BatchNormalization(
