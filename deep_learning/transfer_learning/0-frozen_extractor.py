2d741f9568c0eebdb1cd6e04a65bc317464dd05e
Salua-Sa <slnulx5@gmail.com>
2026-09-04 11:11:57 +0200

    Add task 0-frozen_extractor.py

---
+++ deep_learning/transfer_learning/0-frozen_extractor.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+This module loads the MobileNetV2 model
+and uses it as a feature extractor.
+"""
+from tensorflow import keras
+
+
+def build_feature_extractor():
+    """
+    Loads a pretrained CNN model (e.g., MobileNetV2)
+    from Keras applications, removes its classification head,
+    and freezes its weights.
+
+    The function should:
+    - Load MobileNetV2 with weights="imagenet",
+    - input_shape=(224, 224, 3) and without it's classification head
+    - Freeze the base model
+    - Add a GlobalAveragePooling2D layer on top
+
+    Returns:
+        Keras Model that outputs features from input images
+        using the frozen base model.
+    """
+    base_model = keras.applications.MobileNetV2(
+        weights="imagenet",
+        input_shape=(224, 224, 3),
+        include_top=False
+        )
+
+    base_model.trainable = False
+
+    inputs = keras.Input(shape=(224, 224, 3))
+    x = base_model(inputs, trainig=False)
+    outputs = keras.layersGlobalAveragePooling2D()(x)
+
+    model = keras.Model(inputs, outputs)
+
+    return model
