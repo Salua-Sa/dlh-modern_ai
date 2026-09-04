1987d6bf9638548998e4eece154907e147620416
Salua-Sa <slnulx5@gmail.com>
2026-09-04 19:52:47 +0200

    Add task 3-data_aug.py

---
+++ deep_learning/transfer_learning/3-data_aug.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+This Module creates a Keras Sequential model containing common
+image data augmentation operations.
+"""
+from tensorflow import keras
+
+
+def build_data_augmentation():
+    """
+    Create a Keras Sequential model containing common image
+    data augmentation operations. This augmentation will be
+    applied to training images before they are passed into
+    the pretrained CNN.
+
+    The function should:
+    - Create a tf.keras.Sequential model
+    - Add the following augmentation layers:
+    - RandomFlip("horizontal")
+    - RandomRotation(0.15)
+    - RandomZoom(0.15)
+    - RandomContrast(0.1)
+
+    Returns:
+        Sequential augmentation model
+    """
+    data_augmentation = tf.keras.Sequential(
+        [
+            keras.layers.RandomFlip("horizontal", seed=42),
+            keras.layers.RandomRotation(0.15, seed=42),
+            keras.layers.RandomZoom(0.15, seed=42),
+            keras.layers.RandomContrast(0.1, seed=42)
+            ]
+        )
+
+    return data_augmentation
