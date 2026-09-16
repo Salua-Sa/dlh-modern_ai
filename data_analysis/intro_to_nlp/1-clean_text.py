759bf6221f99f50e5e90d718113bbcc968eec7f8
Salua-Sa <slnulx5@gmail.com>
2026-09-16 14:19:29 +0200

    Fix task 1-clean_text.py

--- data_analysis/intro_to_nlp/1-clean_text.py
+++ data_analysis/intro_to_nlp/1-clean_text.py
@@ -34,10 +34,8 @@ def clean_text(text, replace_num=True,
     Clean and normalize an SMS message.
     """
     # Return "" for None or not str input
-    if text is None:
+    if text is None or not isinstance(text, str)::
         return ""
-    if not isinstance(text, str):
-        return " "
 
     # Convert to lowercase and strip leading/trailing whitespace
     text = text.lower().strip()
