93cda993ac8d193360530fea9b2978cf6d1a2030
Salua-Sa <slnulx5@gmail.com>
2026-09-16 15:38:06 +0200

    Fix task 4-filter_tokens.py

--- data_analysis/intro_to_nlp/4-filter_tokens.py
+++ data_analysis/intro_to_nlp/4-filter_tokens.py
@@ -34,7 +34,7 @@ def filter_tokens(tokens, min_len=2, strip_hashtag=False):
 
         # If strip_hashtag=True and the token starts with #,
         # strip the # prefix before continuing
-        if strip_hashtag and token.startwith("#"):
+        if strip_hashtag and token.starstwith("#"):
             token = token[1:]
 
         # Drop tokens shorter than min_len.
