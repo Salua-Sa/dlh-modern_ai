bd89d2ac7a268b423404bd4a6ea837c1baf944af
Salua-Sa <slnulx5@gmail.com>
2026-09-21 21:35:43 +0200

    Fix task 2-get_mask_index.py

--- deep_learning/llm/2-get_mask_index.py
+++ deep_learning/llm/2-get_mask_index.py
@@ -21,12 +21,12 @@ def get_mask_index(inputs, tokenizer):
     mask_token_id = tokenizer.mask_token_id
     mask_indices = []
 
-    # The function searches through the input token IDs and extracts the indices where mask tokens appear
+    # The function searches through the input token IDs
     for index, token_id in enumerate(token_ids):
         if token_id.item() == mask_token_id:
             mask_indices.append(index)
-        # If no mask token is found, the function must raise a ValueError
-        if not mask_indices:
-            raise ValueError("No <mask> token found in the input!")
+    # If no mask token is found, the function must raise a ValueError
+    if not mask_indices:
+        raise ValueError("No <mask> token found in the input!")
 
     return mask_indices
