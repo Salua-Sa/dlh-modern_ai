c6cedd2ca928634ce569e8405a05a2720e190a54
Salua-Sa <slnulx5@gmail.com>
2026-09-22 12:41:34 +0200

    Add task 3-compute_mask_logits.py

---
+++ deep_learning/llm/3-compute_mask_logits.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" This module  computes the raw logits for all <mask> tokens in a
+tokenized sentence using a pre-trained RoBERTa masked language model.
+"""
+
+
+def compute_mask_logits(model, inputs, mask_indices):
+    """
+    Compute the raw logits for all <mask> tokens in a tokenized sentence
+    using a pre-trained RoBERTa masked language model.
+
+
+    Args:
+        model: A RobertaForMaskedLM model loaded with pre-trained weights.
+        inputs: Tokenized inputs.
+        mask_indices: Positions of all <mask> tokens in the input sequence.
+
+    Returns:
+        mask_logits_list: A list containing logits tensors
+                          for each <mask> token.
+    """
+    outputs = model(**inputs)
+    logits = outputs.logits
+    mask_logits_list = []
+
+    for mask_index in mask_indices:
+        mask_logits = logits[0, mask_index]
+
+        mask_logits_list.append(mask_logits)
+
+    return mask_logits_list
